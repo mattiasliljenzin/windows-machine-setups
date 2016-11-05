76e55597f80a9d1521015c039c3c9870bcd738c5
Mattias Liljenzin <mattias.liljenzin@gmail.com>
2016-11-05 10:04:11 +0100

    Update 01-dotnet-development-machine.ps1
    alignment + added slack

--- 01-dotnet-development-machine.ps1
+++ 01-dotnet-development-machine.ps1
@@ -15,21 +15,24 @@ cinst visualstudiocode
 cinst SublimeText3
 cinst SublimeText3.PackageControl
 
+# Social
+cinst slack
+
 # NodeJS
-choco install nodejs.install
+cinst nodejs.install
 
 # Utilities
 cinst fiddler4
 cinst f.lux
 cinst 7zip
-choco install curl
-choco install windirstat
+cinst curl
+cinst windirstat
 
 # SQL
 cinst mssqlservermanagementstudio2014express
 
 # Visual Studio
-cinst VisualStudio2015Enterprise -y -InstallArguments WebTools
+cinst VisualStudio2015Professional -y -InstallArguments WebTools
 if (Test-PendingReboot) { Invoke-Reboot }
 
 # Plugins
